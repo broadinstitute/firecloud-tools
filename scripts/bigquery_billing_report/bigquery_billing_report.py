4bf9f5598d13634ad2d6dfe788a05d77452e957e
abaumann <abaumann@broadinstitute.org>
2017-11-02 13:02:12 -0400

    Added ability to specify different project to bill query to

--- scripts/bigquery_billing_report/bigquery_billing_report.py
+++ scripts/bigquery_billing_report/bigquery_billing_report.py
@@ -2,32 +2,34 @@
 from common import *
 
 
-def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow_id, show_all_calls, dataset_project_name, dataset_name):
-    print "Retrieving submissions in workspace..."       
+def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow_id, show_all_calls,
+                             bill_query_to_project_name, dataset_project_name, dataset_name, print_queries):
+    print "Retrieving submissions in workspace..."
     workspace_request = firecloud_api.get_workspace(ws_namespace, ws_name)
-    
+
     if workspace_request.status_code != 200:
         fail("Unable to find workspace: %s/%s --- %s" % (ws_namespace, ws_name, workspace_request.text))
-        
+
     submissions_json = firecloud_api.list_submissions(ws_namespace, ws_name).json()
-    
+
     workflow_dict = {}
     submission_dict = {}
     for submission_json in submissions_json:
         sub_id = submission_json["submissionId"]
-        
+
         if query_sub_id and sub_id not in query_sub_id:
             continue;
-        
+
         sub_details_json = firecloud_api.get_submission(ws_namespace, ws_name, sub_id).json()
         submission_dict[sub_id] = sub_details_json
 
-        for wf in (wf for wf in sub_details_json["workflows"] if "workflowId" in wf and "workflows" in sub_details_json):
+        for wf in (wf for wf in sub_details_json["workflows"] if
+                   "workflowId" in wf and "workflows" in sub_details_json):
             wf_id = wf["workflowId"]
             if query_workflow_id and wf_id not in query_workflow_id:
                 continue;
 
-            workflow_dict[wf_id] = {"submission_id":sub_id, "workflow":wf}
+            workflow_dict[wf_id] = {"submission_id": sub_id, "workflow": wf}
 
     firecloud_api._fiss_access_headers = _fiss_access_headers_local
     if len(workflow_dict) == 0:
@@ -41,7 +43,7 @@ def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow
 
     print "Gathering pricing data..."
 
-    client = bigquery.Client(dataset_project_name)
+    client = bigquery.Client(bill_query_to_project_name)
     dataset = client.dataset(dataset_name)
 
     matched_workflow_ids = set()
@@ -55,65 +57,66 @@ def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow
         subquery_subset = subquery_list[query_index_start:query_index_end]
         workflows_subquery = " OR ".join(subquery_subset)
 
-        for table in dataset.list_tables():
-            query = """
-                                 SELECT GROUP_CONCAT(labels.key) WITHIN RECORD AS labels_key,
-                                        GROUP_CONCAT(labels.value) WITHIN RECORD labels_value,
-                                        cost,
-                                        product,
-                                        resource_type
-                                 FROM %s.%s
-                                 WHERE project.id = '%s'
-                                         AND
-                                       labels.key IN ("cromwell-workflow-id",
-                                                      "cromwell-workflow-name",
-                                                      "cromwell-sub-workflow-name",
-                                                      "wdl-task-name",
-                                                      "wdl-call-alias")
-                                 HAVING %s
-                                 # uncomment for quick testing:
-                                 #LIMIT 1
-                               """ % (dataset.name, table.name, ws_namespace, workflows_subquery)
+        query = """
+                             SELECT GROUP_CONCAT(labels.key) WITHIN RECORD AS labels_key,
+                                    GROUP_CONCAT(labels.value) WITHIN RECORD labels_value,
+                                    cost,
+                                    product,
+                                    resource_type
+                             FROM [%s:%s]
+                             WHERE project.id = '%s'
+                                     AND
+                                   labels.key IN ("cromwell-workflow-id",
+                                                  "cromwell-workflow-name",
+                                                  "cromwell-sub-workflow-name",
+                                                  "wdl-task-name",
+                                                  "wdl-call-alias")
+                             HAVING %s
+                             # uncomment for quick testing:
+                             #LIMIT 1
+                           """ % (dataset_project_name, dataset_name, ws_namespace, workflows_subquery)
+        if print_queries:
             print query
-            query_results = client.run_sync_query(query)
 
-            # Use standard SQL syntax for queries.
-            # See: https://cloud.google.com/bigquery/sql-reference/
-            # query_results.use_legacy_sql = False
+        query_results = client.run_sync_query(query)
 
-            query_results.run()
+        # Use standard SQL syntax for queries.
+        # See: https://cloud.google.com/bigquery/sql-reference/
+        # query_results.use_legacy_sql = False
 
-            print "Retrieving BigQuery cost information for workflows %d to %d of %d..." % (
+        query_results.run()
+
+        print "Retrieving BigQuery cost information for workflows %d to %d of %d..." % (
             query_index_start, min(query_index_end, len(workflow_dict)), len(workflow_dict))
 
-            page_token = None
+        page_token = None
 
-            while True:
-                rows = query_results.fetch_data(
-                    max_results=1000,
-                    page_token=page_token)
-                for row in rows:
-                    total_row_count += 1
+        while True:
+            rows = query_results.fetch_data(
+                max_results=1000,
+                page_token=page_token)
+            for row in rows:
+                total_row_count += 1
 
-                    labels = dict(zip(row[0].split(","), row[1].split(",")))
-                    cost = row[2]
-                    product = row[3]
-                    resource_type = row[4]
+                labels = dict(zip(row[0].split(","), row[1].split(",")))
+                cost = row[2]
+                product = row[3]
+                resource_type = row[4]
 
-                    workflow_id = labels["cromwell-workflow-id"].replace("cromwell-", "")
-                    task_name = labels["wdl-task-name"]
+                workflow_id = labels["cromwell-workflow-id"].replace("cromwell-", "")
+                task_name = labels["wdl-task-name"]
 
-                    if "wdl-call-alias" not in labels:
-                        call_name = task_name
-                    else:
-                        call_name = labels["wdl-call-alias"]
-                    workflow_id_to_cost[workflow_id].append(
-                        CostRow(cost, product, resource_type, workflow_id, task_name, call_name))
-                    if workflow_id in workflow_dict:
-                        matched_workflow_ids.add(workflow_id)
+                if "wdl-call-alias" not in labels:
+                    call_name = task_name
+                else:
+                    call_name = labels["wdl-call-alias"]
+                workflow_id_to_cost[workflow_id].append(
+                    CostRow(cost, product, resource_type, workflow_id, task_name, call_name))
+                if workflow_id in workflow_dict:
+                    matched_workflow_ids.add(workflow_id)
 
-                if not page_token:
-                    break
+            if not page_token:
+                break
 
     submission_id_to_workflows = defaultdict(list)
     for wf_id in workflow_dict:
@@ -149,8 +152,6 @@ def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow
             if "calls" not in workflow_metadata_json:
                 # print ws_namespace, ws_name, submission_id, wf_id, workflow_metadata_json
                 continue
-            # print json.dumps(workflow_metadata_json, indent=4, sort_keys=True)
-            print workflow_metadata_json["calls"].keys()
 
             # workflow_metadata_json = workflow_id_to_metadata_json[wf_id]
             calls_lower_json = dict(
@@ -172,7 +173,6 @@ def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow
                     # TODO: this is not a complete implementation - however I requested that cromwell metadata includes label
                     # TODO: so that this translation is not necessary
                     cromwell_translated_callname = re.sub("[^a-z0-9\-]", "-", call_name.lower())
-                    print call_name, cromwell_translated_callname
                     calls_lower_translated_json[cromwell_translated_callname] = call_json
 
             print "|\t.--- Workflow %d of %d: %s (%s)" % (wf_count, wf_total_count, wf_id, workflow_json["status"])
@@ -187,8 +187,6 @@ def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow
             cpu_cost = 0.0
             other_cost = 0.0
             for call_name in call_name_to_cost:
-                print call_name, calls_lower_json.keys(), calls_lower_translated_json.keys()
-
                 calls = calls_lower_json[call_name] if call_name in calls_lower_json else calls_lower_translated_json[
                     call_name]
 
@@ -215,7 +213,7 @@ def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow
                         preempted = "[preempted]" if call["preemptible"] == True and call[
                                                                                          "executionStatus"] == "RetryableFailure" else ""
                         print "|\t|\t             * Attempt #%d: start: %s - end: %s (elapsed: %s) %s" % (
-                        call["attempt"], start, end, end - start, preempted)
+                            call["attempt"], start, end, end - start, preempted)
 
                 sorted_costs = sorted(resource_type_to_pricing, key=lambda rt: resource_type_to_pricing[rt],
                                       reverse=True)
@@ -242,7 +240,7 @@ def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow
 
             print "|\t|    %s" % ("-" * 100)
             print "|\t'--> Workflow Cost: $%f (cpu: $%f | disk: $%f | other: $%f)\n|" % (
-            total, cpu_cost, pd_cost, other_cost)
+                total, cpu_cost, pd_cost, other_cost)
 
             submission_total += total
             submission_pd_cost += pd_cost
@@ -250,24 +248,24 @@ def print_submission_pricing(ws_namespace, ws_name, query_sub_id, query_workflow
             submission_other_cost += other_cost
 
         # only a single workflow
-        if query_workflow_id!=None and len(query_workflow_id) > 0:
+        if query_workflow_id != None and len(query_workflow_id) > 0:
             print "'%s" % ("-" * 100)
         else:
             print "|    %s" % ("-" * 100)
             missing_workflows = len(workflow_ids_with_no_cost) > 0
             caveat_text = (" (** for %d out of %d workflows)") % (
-            wf_count - len(workflow_ids_with_no_cost), wf_count) if missing_workflows else ""
+                wf_count - len(workflow_ids_with_no_cost), wf_count) if missing_workflows else ""
             print "'--> Submission Cost%s: $%f (cpu: $%f | disk: $%f | other: $%f)\n" % (
-            caveat_text, submission_total, submission_cpu_cost, submission_pd_cost, submission_other_cost)
+                caveat_text, submission_total, submission_cpu_cost, submission_pd_cost, submission_other_cost)
             if missing_workflows:
                 print "     ** %d workflows without cost information, e.g. %s" % (
-                len(workflow_ids_with_no_cost), next(iter(workflow_ids_with_no_cost)))
+                    len(workflow_ids_with_no_cost), next(iter(workflow_ids_with_no_cost)))
 
 
 class CostRow():
     def __init__(self, cost, product, resource_type, workflow_id, task_name, call_name):
         self.cost = cost
-        self.product = product 
+        self.product = product
         self.resource_type = resource_type
         self.workflow_id = workflow_id
         self.task_name = task_name
@@ -303,7 +301,7 @@ def _fiss_access_headers_local(headers=None):
     """
     credentials = GoogleCredentials.get_application_default()
     access_token = get_token()
-    fiss_headers = {"Authorization" : "bearer " + access_token}
+    fiss_headers = {"Authorization": "bearer " + access_token}
     fiss_headers["User-Agent"] = firecloud_api.FISS_USER_AGENT
     if headers:
         fiss_headers.update(headers)
@@ -317,16 +315,27 @@ def main():
     parser = DefaultArgsParser(description="Print a report about costs for a given submission.")
 
     # Core application arguments
-    parser.add_argument('-p', '--namespace', dest='ws_namespace', action='store', required=True, help='Workspace namespace')
+    parser.add_argument('-p', '--namespace', dest='ws_namespace', action='store', required=True,
+                        help='Workspace namespace')
     parser.add_argument('-n', '--name', dest='ws_name', action='store', required=True, help='Workspace name')
 
-    parser.add_argument('-s', '--subid', dest='submission_id', action='store', required=False, help='Optional Submission ID to limit the pricing breakdown to.  If not provided pricing for all submissions in this workspace will be reported.')
-    parser.add_argument('-w', '--wfid', dest='workflow_id', default=[], action='append', required=False, help='Optional Workflow ID to limit the pricing breakdown to.  Note that this requires a submission id to be passed as well."')
+    parser.add_argument('-s', '--subid', dest='submission_id', action='store', required=False,
+                        help='Optional Submission ID to limit the pricing breakdown to.  If not provided pricing for all submissions in this workspace will be reported.')
+    parser.add_argument('-w', '--wfid', dest='workflow_id', default=[], action='append', required=False,
+                        help='Optional Workflow ID to limit the pricing breakdown to.  Note that this requires a submission id to be passed as well."')
 
-    parser.add_argument('-c', '--calls', dest='show_all_calls', action='store_true', required=False, help='Expand information about each call.')
+    parser.add_argument('-c', '--calls', dest='show_all_calls', action='store_true', required=False,
+                        help='Expand information about each call.')
 
-    parser.add_argument('-dp', '--dataset_project', dest='dataset_project', action='store', required=False, help='Optional project where dataset is stored - defaults to the workspace project.')
-    parser.add_argument('-dn', '--dataset_name', dest='dataset_name', action='store', required=False, help='Optional dataset name where billing data is stored - defaults to billing_export.')
+    parser.add_argument('-bp', '--bill_to_project', dest='bill_to_project', action='store', required=False,
+                        help='Optional project to bill this Big Query query to.  This defaults to the workspace namespace if not provided.')
+    parser.add_argument('-dp', '--dataset_project', dest='dataset_project', action='store', required=False,
+                        help='Optional project where dataset is stored - defaults to the workspace project.  This defaults to the workspace namespace if not provided.')
+    parser.add_argument('-dn', '--dataset_name', dest='dataset_name', action='store', required=False,
+                        help='Optional dataset name where billing data is stored - defaults to billing_export.')
+
+    parser.add_argument('-pq', '--print_queries', dest='print_queries', action='store_true', required=False,
+                        help='Optional flag to cause all the BigQuery queries to be printed.')
 
     args = parser.parse_args()
 
@@ -337,8 +346,10 @@ def main():
         print "Note that this script expects the billing export table to be named 'billing_export'.  "
 
     print_submission_pricing(args.ws_namespace, args.ws_name, args.submission_id, args.workflow_id, args.show_all_calls,
-                args.dataset_project if args.dataset_project else args.ws_namespace,
-                args.dataset_name if args.dataset_name else 'billing_export')
+                             args.bill_to_project if args.bill_to_project else args.ws_namespace,
+                             args.dataset_project if args.dataset_project else args.ws_namespace,
+                             args.dataset_name if args.dataset_name else 'billing_export',
+                             args.print_queries)
 
 
 if __name__ == "__main__":
