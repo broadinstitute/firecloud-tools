65ed1f0bdcac10ae2da425a2bfaa24eb56058205
abaumann <abaumann@broadinstitute.org>
2018-03-07 16:38:02 -0500

    Added scopes for app default creds in get_access_token.py, which allows a service account that is the app default to get the right scopes

--- scripts/get_access_token.py
+++ scripts/get_access_token.py
@@ -2,14 +2,18 @@ import sys
 import os
 from oauth2client.client import GoogleCredentials
 
+# scopes needed to get through the proxy - only important for service accounts where they aren't the default
+scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
 
 if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
     from oauth2client.service_account import ServiceAccountCredentials
-    scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
+    
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         sys.argv[1], scopes=scopes)
 elif len(sys.argv) > 1 and sys.argv[1] == "default":
     credentials = GoogleCredentials.get_application_default()
+    # in case the app default credentials are for a service account, we set the scopes
+    credentials.scopes=scopes
 else:
     print "This script will print out an access token for either the application default credentials or for a service account.  " \
           "\nOnly the token is printed so this can be combined with other commands such as curl calls."
