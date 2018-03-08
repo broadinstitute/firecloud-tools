aec182c6f26295b36e2d3a28abbcec26035fa46c
abaumann <abaumann@broadinstitute.org>
2018-03-07 20:06:16 -0500

    changed to setting scopes by create_scoped

--- scripts/get_access_token.py
+++ scripts/get_access_token.py
@@ -1,6 +1,8 @@
 import sys
 import os
 from oauth2client.client import GoogleCredentials
+import google.auth
+from google.auth.transport.requests import AuthorizedSession
 
 # scopes needed to get through the proxy - only important for service accounts where they aren't the default
 scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
@@ -11,9 +13,8 @@ if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         sys.argv[1], scopes=scopes)
 elif len(sys.argv) > 1 and sys.argv[1] == "default":
-    credentials = GoogleCredentials.get_application_default()
+    credentials = GoogleCredentials.get_application_default().create_scoped(scopes)
     # in case the app default credentials are for a service account, we set the scopes
-    credentials.scopes=scopes
 else:
     print "This script will print out an access token for either the application default credentials or for a service account.  " \
           "\nOnly the token is printed so this can be combined with other commands such as curl calls."
