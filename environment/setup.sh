183a655ad23071d7a1cc1bf7b95088bd2338cb82
Kate Voss <kvoss@broadinstitute.org>
2018-03-16 18:02:14 -0400

    Added config output and test of config, incomplete

--- environment/setup.sh
+++ environment/setup.sh
@@ -29,7 +29,7 @@ if [ ! -d ~/.firecloud-env.config ]; then
     fi
     echo
     echo
-    while read -p "Do you have an existing Google project where you want to run Cromwell workflows? (yes or no) " yn; do
+    while read -p "Do you have an existing Google project where you want to run workflows? (yes or no) " yn; do
         #Which Google project to use (new or existing) 
         case $yn in
             [Yy]* ) read -p "Enter your project name: " project; break;;
@@ -100,10 +100,143 @@ if [ ! -d ~/.firecloud-env.config ]; then
     echo "Bucket created for Cromwell execution outputs can be viewed at: https://console.cloud.google.com/storage/browser/$bucket"
     #TODO: ask for dockerhub credentials if they are going to use private dockers
 
-    #TODO: create config with: execution bucket, project, optional Dockerhub credentials
+    #Create config
+    echo "include required(classpath(\"application\"))
 
-    echo $project  
-    #read varname
+google {
+
+  application-name = \"cromwell\"
+
+  auths = [
+    {
+      name = \"application-default\"
+      scheme = \"application_default\"
+    }
+  ]
+}
+
+engine {
+  filesystems {
+    gcs {
+      auth = \"application-default\"
+    }
+  }
+}
+
+backend {
+  default = \"JES\"
+  providers {
+    JES {
+      actor-factory = \"cromwell.backend.impl.jes.JesBackendLifecycleActorFactory\"
+      config {
+        // Google project
+        project = \"$project\"
+        compute-service-account = \"default\"
+
+        // Base bucket for workflow executions
+        root = \"gs://$bucket\"
+
+        // Polling for completion backs-off gradually for slower-running jobs.
+        // This is the maximum polling interval (in seconds):
+        maximum-polling-interval = 600
+
+        // Optional Dockerhub Credentials. Can be used to access private docker images.
+        dockerhub {
+          // account = \"\"
+          // token = \"\"
+        }
+
+        genomics {
+          // A reference to an auth defined in the `google` stanza at the top.  This auth is used to create
+          // Pipelines and manipulate auth JSONs.
+          auth = \"application-default\"
+          // Endpoint for APIs, no reason to change this unless directed by Google.
+          endpoint-url = \"https://genomics.googleapis.com/\"
+        }
+
+        filesystems {
+          gcs {
+            // A reference to a potentially different auth for manipulating files via engine functions.
+            auth = \"application-default\"
+          }
+        }
+      }
+    }
+  }
+}   " > google.conf
+    echo
+    echo "Your configuration file is ready! It is stored in google.conf."
+    echo
+    echo "To use this configuration you will need to enable the following APIs:"
+    echo "Google Cloud Storage, Google Compute Engine, Google Genomics."
+    while read -p "Would you like to enable these APIs now? (yes or no)" yn; do
+        #enable APIs, or not
+        case $yn in
+            [Yy]* ) 
+                #TODO list configu, store current project that user is authenticated with, change project to enable APIs, then undo after 
+                #gcloud config list
+                #gcloud config set project $project
+                gcloud services enable compute.googleapis.com genomics.googleapis.com storage-component.googleapis.com
+                break;;
+
+            [Nn]* )
+                echo "Don't forget to enable the APIs through the Google Console or gcloud SDK prior to using the configuration."
+                echo "Exiting."
+                exit 1 
+                break;;
+
+            * ) echo "Please answer yes or no.";;
+        #enable APIs, or not
+        esac 
+    done
+    while read -p "Do you want to run a Hello WDL test to check your configuration? (yes or no)" yn; do
+        #Test configuration
+        case $yn in
+            [Yy]* ) 
+                #create WDL
+                echo "task hello {
+  String addressee  
+  command {
+    echo \"Hello ${addressee}! Welcome to Cromwell . . . on Google Cloud!\"  
+  }
+  output {
+    String message = read_string(stdout())
+  }
+  runtime {
+    docker: \"ubuntu:latest\"
+  }
+}
+
+workflow wf_hello {
+  call hello
+
+  output {
+     hello.message
+  }
+}
+" > hello.wdl
+
+                #create inputs
+                echo "{
+  \"wf_hello.hello.addressee\": \"World\"
+}
+" > hello.inputs
+
+                #TODO: download Cromwell
+
+
+                break;;
+
+            [Nn]* )
+                echo "Exiting."
+                exit 1 
+                break;;
+
+            * ) echo "Please answer yes or no.";;
+        #Test configuration
+        esac 
+    done    
+
 else
     echo "Setup has already been done.  If you would like to clear this setup and create"
     echo "a new one, you can remove the file ~/.firecloud-env.config"
