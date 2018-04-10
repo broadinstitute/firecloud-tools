3f00e2f60de67b79e40159b58a7bad4e85314b59
Kate Voss <kvoss@broadinstitute.org>
2018-04-10 13:47:56 -0400

    Removed cromwell.sh and env.sh

--- environment/cromwell.sh
+++
@@ -1,7 +0,0 @@
-$ ./cromwell.sh local hello.wdl hello.inputs
-$ # runs: java -jar cromwell.jar run hello.wdl -i hello.inputs
-$ ./cromwell.sh google hello.wdl hello.inputs
-$ # runs: java -Dconfig.file=google.conf -jar cromwell.jar run hello.wdl -i hello.inputs
-
-$1 and $2 to access argv
-
