43f1a7978aacd331b060ba031325acc40e8226f6
Kate Voss <kvoss@broadinstitute.org>
2018-05-10 14:37:27 +0200

    Checkpoint, dev continued in katevoss/cs-50

--- environment/setup.py
+++ environment/setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
 
 setup(name='Cromwell Config setup',
       version='1.0',
-      description='Setup For Cromwell Configuration to run on Google Cloud',
+      description='Setup an individual configuration file to run Cromwell on Google Cloud',
       author='Kate Voss',
       author_email='kvoss@broadinstitute.org',
       url='https://github.com/broadinstitute/firecloud-tools',
