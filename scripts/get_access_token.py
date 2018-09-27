0e831143a1983277ec62536d96cdbf4505f2c5a5
Andrew <ajang@broadinstitute.org>
2018-09-27 13:46:38 -0400

    parameterize additional scopes

--- scripts/get_access_token.py
+++ scripts/get_access_token.py
@@ -6,6 +6,15 @@ from oauth2client.client import GoogleCredentials
 if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
     from oauth2client.service_account import ServiceAccountCredentials
     scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
+    if len(sys.argv) > 2:
+      newScopes = eval(sys.argv[2])
+      if isinstance(newScopes, list):
+        scopes = scopes + newScopes
+      else:
+        print "If you want to add additional scopes, add this in the form of a list."
+        print "For example: " \
+              "\n\tpython get_access_token.py <credentials/default> \"['https://www.googleapis.com/auth/cloud-platform']\""
+        exit(1)
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         sys.argv[1], scopes=scopes)
 elif len(sys.argv) > 1 and sys.argv[1] == "default":
